1bd9a126c09f974002a4b1b37e9e87f5dff58cf1
jayreliution999 <jay.reliution@gmail.com>
2022-10-10 18:46:13 +0530

    exercise_2 file added

--- exercise_2.py
+++ exercise_2.py
@@ -1,8 +1,11 @@
 class Category:
-    def __init__(self, name, code, parent=None):
+    code = 0
+    def __init__(self, name, parent=None):
         self.name = name
-        self.code = code
+        self.code = Category.code + 1
+        Category.code = Category.code + 1
         self.no_of_product = 0
+        self.product = []
         self.parent = parent
         self.display_name = self.name
         self.display_name1()
@@ -12,104 +15,85 @@ class Category:
         print("CategoryCode : ", self.code)
         print("dispaly_name : ", self.display_name)
         print("no of products:", self.no_of_product)
-        print("ParentCategory : ", self.parent)
-        print("\n")
+        print()
 
     def display_name1(self):
         count = self
         while (count.parent != None):
-                count.display_name = f'{count.parent.name} > {self.display_name}'
+                self.display_name = f'{count.parent.name} > {self.display_name}'
                 count = count.parent
 
-
 class Product:
-    def __init__(self, name, code, category, price):
+    codep = 0
+    def __init__(self, name, category, price):
         self.name = name
-        self.code = code
-        self.category = category
+        self.code = Product.codep + 1
+        Product.codep = Product.codep + 1
+        self.category = category.name
+        self.no_of_product = self.code + 1
         category.no_of_product = category.no_of_product + 1
         self.price = price
 
     def display_product(self):
         print("ProductName : ", self.name)
         print("ProductCode : ", self.code)
-        print("Category : ", self.category.name)
+        print("Category : ", self.category)
         print("Price : ", self.price)
         print("\n")
 
 
 #Parent Object
 
-plastic = Category("plastic", 1)
-chocolate = Category("chocolate", 2)
-vehicle = Category("vehicle", 3)
-electric = Category("electric", 4)
-chemical = Category("chemical", 5)
+plastic = Category("plastic")
+chocolate = Category("chocolate")
+vehicle = Category("vehicle")
+electric = Category("electric")
+chemical = Category("chemical")
 
 #Child Object
 
-c1 = Category("Tupperwere", 1, plastic)
-c2 = Category("Dark Chocolate", 2, chocolate)
-c3 = Category("Four Wheelers", 3, vehicle)
-c4 = Category("DC", 4, electric)
-c5 = Category("Paint", 5, chemical)
+c1 = Category("Tupperwere", plastic)
+c2 = Category("Dark Chocolate", chocolate)
+c3 = Category("Four Wheelers", vehicle)
+c4 = Category("DC", electric)
+c5 = Category("Paint", chemical)
 
 #Child of Child Object
 
-childofc1 = Category("Water container", 1, c1)
-childofc2 = Category("Amul", 2, c2)
-childofc3 = Category("Audi", 3, c3)
+childofc1 = Category("Water container", c1)
+childofc2 = Category("Amul", c2)
+childofc3 = Category("Audi", c3)
 
 #List of Products
 
-pro1 = Product("Milk bottle", 1, plastic, 225)
-pro2 = Product("Lily’s Dark", 2, chocolate, 100)
-pro3 = Product("Audi Q3", 3, vehicle, 1200000)
-pro4 = Product("Lindt Dark", 2, chocolate, 400)
-
-pro5 = Product("Wet food storage", 1, plastic, 300)
-pro6 = Product("Audi A8", 3, vehicle, 2500000)
-pro7 = Product("Hu Simple Dark", 2, chocolate, 500)
-pro8 = Product("Audi Q8", 3, vehicle, 3500000)
-
-pro9 = Product("Kitchen helper", 1, plastic, 3000)
-pro10 = Product("Audi A6", 3, vehicle, 4500000)
-pro11 = Product("Chocolove Dark", 2, chocolate, 600)
-pro12 = Product("Freezer Storage", 1, plastic, 450)
-
-pro13 = Product("Fan", 4, electric, 1250)
-pro14 = Product("TubeLight", 4, electric, 45)
-
-pro15 = Product("Asian Paint", 5, chemical, 850)
-pro16 = Product("Dulux Paint", 5, chemical, 650)
+category_list = [plastic, chocolate, vehicle, c1, c2, c3, c4, c5, childofc1, childofc2, childofc3, electric, chemical]
 
 
-c1.display_category()
-c2.display_category()
-c3.display_category()
-c4.display_category()
-c5.display_category()
+product_list = [
+    Product("Water bottle", c1, 225),
+    Product("Lily’s Dark", c2, 100),
+    Product("Audi Q3", c3, 1200000),
+    Product("Lindt Dark", c2, 400),
+    Product("Wet food storage", c1, 300),
+    Product("Audi A8", c3, 2500000),
+    Product("Hu Simple Dark", c2, 500),
+    Product("Audi Q8", c3, 3500000),
+    Product("Kitchen helper", c1, 3000),
+    Product("Audi A6", c3, 4500000),
+    Product("Chocolove Dark", c2, 600),
+    Product("Freezer Storage", c1, 450),
+    Product("Fan", c4, 4500),
+    Product("i20", vehicle, 340000),
+    Product("Fruit & Nuts", chocolate, 340),
+    Product("Light & series", electric, 940),
+    Product("Tubelight", electric, 125),
+    Product("Asian Paint", chemical, 3000),
+    Product("Dulux Paint", chemical, 2500),
+    Product("Super Paint", chemical, 1000),
+]
 
-
-pro1.display_product()
-pro2.display_product()
-pro3.display_product()
-pro4.display_product()
-pro5.display_product()
-pro6.display_product()
-pro7.display_product()
-pro8.display_product()
-pro9.display_product()
-pro10.display_product()
-pro11.display_product()
-pro12.display_product()
-pro13.display_product()
-pro14.display_product()
-pro15.display_product()
-pro16.display_product()
-
-
-
-
-
+for category in category_list:
+    category.display_category()
+for product in product_list:
+    product.display_product()
 
