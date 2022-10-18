2a2e9fe44183041c4408038bc5c845e61726b3ca
jayreliution999 <jay.reliution@gmail.com>
2022-10-18 18:34:11 +0530

    1 file added ex-3

--- exercise_3.py
+++ exercise_3.py
@@ -12,8 +12,10 @@ class Location:
         Location.code_location = Location.code_location + 1
 
     def display_1(self):
+        print("----------------------------")
         print("Location Name : ", self.name)
         print("Location Code : ", self.code)
+        print("----------------------------")
 
 
 class Movement:
@@ -22,16 +24,18 @@ class Movement:
         self.to_location = to_location
         self.product = product
         self.quantity = quantity
-
-    def display_movement(self):
-        print("FromLocation : ", self.from_location)
-        print("To Location : ", self.to_location)
-        print("Product Name :", self.product)
-        print("How many Quantity : ", self.quantity)
+        self.display = f'quantity of product : {self.quantity} from {self.from_location.name} to {self.to_location.name}'
 
     @staticmethod
     def movements_by_product(product):
-        pass
+        flag = 0
+        for item in listof_movement:
+            if item.product.name == product.name:
+                flag = 1
+                print(item.display)
+        if flag == 0:
+            print("No Movement of product")
+
 
 #Location of object
 
@@ -48,6 +52,7 @@ for i in listof_location:
 
 chemical = Category("Chemical")
 
+
 soap = Product("Soap", chemical, 100, {rajkot: 40, ahemdabad: 20, baroda: 10})
 detergent = Product("Detergent", chemical, 75, {ahemdabad: 10, surat: 20, baroda: 40})
 toothpaste = Product("Toothpaste", chemical, 45, {rajkot: 50, baroda: 30, ahemdabad: 20})
@@ -59,7 +64,7 @@ for i in listof_product:
     print(i.name)
     for j in i.stock_at_location: #print dictionary of location name,quntity value
         print(f'{j.name} -> {i.stock_at_location[j]}')
-
+    print()
 
 
 #Move 5 Products from one location to another location
@@ -71,4 +76,19 @@ movement4 = Movement(baroda, surat, chalk, 4)
 movement5 = Movement(ahemdabad, surat, marble, 0)
 
 
+listof_movement = [movement1, movement2, movement3, movement4, movement5]
+
+for i in listof_product:
+    print(i.name)
+    Movement.movements_by_product(i)
+
+print("----------------------------")
+print("product list by location : ")
+print("----------------------------")
+for i in listof_location:
+    print(i.name)
+    for lp in listof_product:
+        if i in lp.stock_at_location:
+            print(f'{lp.name} -> {lp.stock_at_location[i]}')
+    print()
 
